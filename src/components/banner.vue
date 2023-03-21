34d05e0da886bb6a4a6dcb791dbed57dabdb6358
jacobwatua <jacobwatua254@gmai.com>
2023-03-21 21:45:00 +0300

    More responsiveness

--- src/components/banner.vue
+++ src/components/banner.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="content-card">
-    <div class="box">
-    </div>
+    <div class="box1"></div>
     <div class="content">
-        <p class="tex-center">
-          This is where the best businesses rise to the top! We're your go-to destination
-          for finding the top performers in your industry based on the metrics that matter most. Whether you're looking for
-          the most efficient operations, highest customer satisfaction, or greatest revenue growth, we've got you covered.
-          So come on in, explore our rankings, and discover the businesses that are leading the way!
-        </p>
-        <button class="btn btn-danger">Learn More</button>
+      <div>
+          <p class="tex-center">
+            This is where the best businesses rise to the top! We're your go-to destination
+            for finding the top performers in your industry based on the metrics that matter most. Whether you're looking for
+            the most efficient operations, highest customer satisfaction, or greatest revenue growth, we've got you covered.
+            So come on in, explore our rankings, and discover the businesses that are leading the way!
+          </p>
+          <button class="btn btn-danger">Learn More</button>
+      </div>
     </div>
   </div>
 </template>
